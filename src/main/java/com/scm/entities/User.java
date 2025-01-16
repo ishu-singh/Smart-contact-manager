eee6aee8c2d6899c97d43d07b5e602bd890c6f19
ishu singh <ishusingh@ishus-MacBook-Air.local>
2025-01-16 15:26:00 +0530

    login-successfull

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -10,10 +10,21 @@ import jakarta.persistence.Table;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,7 +39,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "user_id", nullable = false, unique = true, length = 36)
@@ -36,6 +47,7 @@ public class User {
     
     @Column(name="user_name",nullable=false)
     private String userName;
+    @Getter(value = AccessLevel.NONE)
     private String userPassword;
     @Column(unique=true,nullable=false)
     private String userEmail;
@@ -45,7 +57,8 @@ public class User {
     private String profilePic;
     private String phoneNumber;
     //other information
-    private boolean enabled=false;
+    @Getter(value = AccessLevel.NONE)
+    private boolean enabled=true;
     private boolean emailVerified=false;
     private boolean phoneVerified=false;
 
@@ -57,6 +70,48 @@ public class User {
     //add more fields if needed
     @OneToMany(mappedBy="user",cascade=CascadeType.ALL,fetch=FetchType.LAZY,orphanRemoval=true)
     private List<Contact> contacts=new ArrayList<>();
+
+    @ElementCollection(fetch=FetchType.EAGER)
+    private List<String> rolesList=new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //list of roles[USER,ADMIN] has been converted to list of SimpleGrantedAuthority
+        Collection<SimpleGrantedAuthority> roles= rolesList.stream().map(role-> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
+        return roles;
+    }
+    // for this project:
+    // email id --> username
+
+    @Override
+    public String getUsername() {
+        return this.userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userPassword;
+    }
 
   
 
--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,9 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +18,22 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserForm {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid Email")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Password must Atleast of 6 characters")
     private String password;
+    @Size(max=100)
+    @NotBlank(message = "About is required")
     private String about;
+    @Size(max=12,min=10,message = "Invalid Phone Number")
     private String phoneNumber;
 
     
